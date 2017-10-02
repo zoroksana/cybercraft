d8e5f1f456ddd3f97febe5262df331449b22f5ba
Oksana Svyryda <erbe@ukr.net>
2017-10-02 08:18:21 +0300

    1

--- app/models/session.rb
+++ app/models/session.rb
@@ -4,14 +4,8 @@ class Session < ApplicationRecord
   def self.import(file)
     CSV.foreach(file.path, headers: true) do |row|
 
-      session_hash = row.to_hash # exclude the price field
-      session = Session.where(id: session_hash["id"])
+      session_hash = row.to_hash
 
-      if session.count == 1
-        session.first.update_attributes(session_hash.except("price")) # exclude the price field.
-      else
-        Session.create!(session_hash)
-      end # end if !product.nil?
-    end # end CSV.foreach
-  end # end self.import(file)
-end # end class
+    end
+    end
+  end
